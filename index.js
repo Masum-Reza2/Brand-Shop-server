be1a40a65d9f45cb21f879d968ae4e07d9d5dc84
Masum-Reza <careerhunter4280@gmail.com>
2023-10-20 15:53:34 -0700

    solved vercel deployment problem

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const database = client.db("brandDB");
         const samsungCollection = database.collection("samsung");
@@ -469,7 +469,7 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
